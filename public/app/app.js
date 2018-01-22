43f640a35ea42c2e5738c14e859b45556cb22447
최낙원 <ch0494@gmail.com>
2018-01-22 09:36:38 +0900

    인증에관한부분 삭제, angular-google-gapi.js 수정부분 제외하고 AngularJS로 전환한 뒤 업로드

---
+++ public/app/app.js
@@ -0,0 +1,57 @@
+var app = angular.module('angular-google-api-example', [
+    'ngCookies',
+    'ui.router',
+    'angular-google-gapi',
+
+    'angular-google-api-example.router',
+    'angular-google-api-example.controller'
+
+]);
+
+app.run(['GAuth', 'GApi', 'GData', '$state', '$rootScope', '$window', '$cookies',
+    function(GAuth, GApi, GData, $state, $rootScope, $window, $cookies) {
+
+        $rootScope.gdata = GData;
+
+        var CLIENT = '615017914706-934pbfeon08pnimorh9vdl8fp3ttf4ir.apps.googleusercontent.com';
+        var BASE;
+        if($window.location.hostname == 'localhost') {
+            BASE = '//localhost:8080/_ah/api';
+        } else {
+            BASE = 'https://cloud-endpoints-gae.appspot.com/_ah/api';
+        }
+
+        BASE = 'https://cloud-endpoints-gae.appspot.com/_ah/api';
+
+        //GApi.load('myContactApi', 'v1', BASE);
+        GApi.load('calendar', 'v3');
+        GAuth.setClient(CLIENT);
+        //GAuth.setScope('https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar.readonly');
+        GAuth.setScope('https://www.googleapis.com/auth/youtube');
+
+        var currentUser = $cookies.get('userId');
+        if(currentUser) {
+            GData.setUserId(currentUser);
+            GAuth.checkAuth().then(
+                function () {
+                    if($state.includes('login'))
+                        $state.go('home');
+                },
+                function() {
+                    $state.go('login');
+                }
+            );
+        } else {
+            $state.go('login');
+        }
+
+
+
+        $rootScope.logout = function() {
+            GAuth.logout().then(function () {
+                $cookies.remove('userId');
+                $state.go('login');
+            });
+        };
+    }
+]);
