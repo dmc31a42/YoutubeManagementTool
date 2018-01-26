bab046c3346a4a83315ea3bbb607bdebaa0e4433
최낙원 <ch0494@gmail.com>
2018-01-26 23:41:49 +0900

    여러 동영상에 공통적으로 있는 문장 추출하는 것 까지 저작자이름과, 제목 수정

--- public/app/controller.home.js
+++ public/app/controller.home.js
@@ -1,7 +1,7 @@
 var controller = angular.module('angular-google-api-example.controller.home', ['ui.sortable','ui.sortable.multiselection']);
 
-controller.controller('angular-google-api-example.controller.home', ['$scope', 'GApi', 'GAuth', 'uiSortableMultiSelectionMethods',
-    function homeCtl($scope, GApi, GAuth, uiSortableMultiSelectionMethods) {
+controller.controller('angular-google-api-example.controller.home', ['$scope', 'GApi', 'GAuth', 'uiSortableMultiSelectionMethods', '$sce',
+    function homeCtl($scope, GApi, GAuth, uiSortableMultiSelectionMethods, $sce) {
 
         GAuth.checkAuth().then(function(){
             GApi.load('youtube', 'v3').then(function() {
@@ -72,11 +72,107 @@ controller.controller('angular-google-api-example.controller.home', ['$scope', '
                 return $(this).index();
             }).toArray();
             console.log(selectedItemIndexes);
-            var selectedItems = $.map(selectedItemIndexes, function(i) {
+            var selectedItems = [];
+            selectedItems = $.map(selectedItemIndexes, function(i) {
                 return $scope.playlistItems[i]
             });
             console.log(selectedItems);
-            $scope.selectedItem = selectedItems;
+            $scope.selectedItems = selectedItems;
+            if($scope.selectedItems.length == 1){
+                $scope.selectedItemVideoURL = $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + $scope.selectedItems[0].snippet.resourceId.videoId);
+            }
+
+            var selectedItemUniqueKinds = $scope.selectedItems.map(function(element){
+                return element.kind;
+            }).filter(function(value, index, self){
+                return self.indexOf(value) === index;
+            });
+            $scope.selectedItemUniqueKinds = selectedItemUniqueKinds;
+           
+           if($scope.selectedItems.length>=2){
+               var titles = $scope.selectedItems.map(function(item){
+                   return item.snippet.title;
+               });
+               determinateSimulality(titles); 
+               function determinateSimulality(array){
+                   var refString = array[0];
+                   var dic = [];
+                   var pos = 0;
+                   var length = 2;
+                   while(pos+length <= refString.length){
+                       var tempString = refString.substring(pos,pos+length);
+                       var tempArray = array.filter(item=>item.includes(tempString));
+                       if(tempArray.length == array.length){
+                           var preDicIndex = dic.indexOf(tempString.substring(0,tempString.length-1));
+                           if(preDicIndex>-1){
+                               dic.splice(preDicIndex, 1);
+                           }
+                           if(!dic.includes(tempString)){
+                               dic.push(tempString);
+                           }
+                           /* pos = pos */
+                           length++;
+                       } else {
+                           if(length==2){
+                               pos++;
+                               /* length = 2 */
+                           } else {
+                               pos = pos+(length-1);
+                               length = 2;
+                           }
+                       }
+                   }
+                   dic.sort(function(a,b){
+                       return b.length-a.length;
+                   });
+                   var tempStrArr = [refString];
+                   for(var i=0; i<dic.length; i++)
+                   {
+                       //var j=0;
+                       //while(j<tempStrArr.length)
+                       for(var j=0; j<tempStrArr.length;){
+                           if(typeof tempStrArr[j] == "string"){
+                               var splitedStr = tempStrArr[j].split(dic[i]);
+                               var tempSplitedStrLength = splitedStr.length;
+                               for(var k=0; k<tempSplitedStrLength-1; k++){
+                                  splitedStr.splice((2*k-1),0,i);
+                               }
+                               if(splitedStr.indexOf("")>-1){
+                                 splitedStr.splice(splitedStr.indexOf(""),1);
+                               }
+                               tempStrArr.splice(i,1);
+                               for(var k=0; k<splitedStr.length; k++){
+                                 tempStrArr.splice(i+k,0,splitedStr[k]);
+                               }
+                               j = j+splitedStr.length;
+                           } else {
+                               j = j+1;
+                           }
+                       }
+                   }
+                   console.log(tempStrArr);
+               };
+           }
+
+
         });
+
+        $scope.ConvertVideoId2URL = function(videoId){
+            if(videoId){
+                return "https://www.youtube.com/embed/" + videoId;
+            } else {
+                return "";
+            }
+        };
     }
 ]);
+
+controller.filter('ConvertVideoId2URL', function(){
+    return function(videoId){
+        if(videoId){
+            return "https://www.youtube.com/embed/" + videoId;
+        } else {
+            return "";
+        }
+    };
+})
